2cdbbc5d222a75046c916dd0cfac0ba8eb60ea78
Félix Martins <felix.marcial@hotmail.com>
2024-12-11 14:50:05 +0000

    Added requirements list to disambiguate used libraries

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='gym_pybullet_drones',
     version='0.6.0',
     install_requires=[
+        'protobuf==3.19.6',
         'numpy==1.20.1',
         'Pillow==8.1.0',
         'matplotlib==3.3.4',
@@ -11,6 +12,6 @@ setup(
         'gym==0.17.3',
         'pybullet==3.0.8',
         'stable_baselines3==0.10.0',
-        'ray[rllib]==0.8.7'
+        'ray[rllib]==0.8.7',
     ]
 )
